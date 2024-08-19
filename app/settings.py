4a63c5b22457eccfde2564477781f9bb1356b0fc
Богдан <41363359+CurlyDen@users.noreply.github.com>
2024-08-19 13:45:55 +0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -1,13 +1,11 @@
 URL = "app3.fundeus.bg/api_mc"
 
 VIBER_SCENARIO = {
-    0: "4fe733525ae7dfe4-b5cba6875b1dad6-2eed53f41039675a",     # Перла
-    1: "5149902f30a7e10e-5a1a26817d06bfe-9b59fea9d3766221",     # Качалка
+    0: "viber_token",    
+    1: "viber_token",     
 }
 
 ME = {
-    0: "2DpjTnaEgk4u74P73jxd2A==",
-    1: "ViTuLWF7hd8CJx5v+P4BEg=="
+    0: "uid",
+    1: "uid"
 }
-
-KEY = 758931352443
